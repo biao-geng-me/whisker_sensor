92b996da76c6f76653b38dd8d86474c23200f77c
Biao Geng <biao.geng.me@gmail.com>
2025-11-12 16:05:08 -0500

    Improve load_datalog

--- data_processing/load_datalog.m
+++ data_processing/load_datalog.m
@@ -1,15 +1,23 @@
 function [dat_table,dt_str,tag] = load_datalog(pathname)
 
-    if ~strcmp(pathname(end-3:end),'.dat')
+    if ~exist(pathname,'file') && ~strcmp(pathname(end-3:end),'.dat')
         pathname = [pathname '.dat'];
     end
-    dat_table = readtable(pathname, "FileType","fixedwidth");
+    try
+        dat_table = readtable(pathname, "FileType","fixedwidth");
+    catch ME
+        error('load_datalog:readtable','Failed to readtable from %s', filepath);
+    end
 
     % extract meta data from datalog filename
     % convention is st_date_time_tag_fields
     [filepath,name,ext] = fileparts(pathname);
-
-    parts=split(name,'_');
-    dt_str = [parts{2} ' ' parts{3}];
-    tag = join(parts(5:end),' ');
-    tag = tag{1};
+    try
+        parts=split(name,'_');
+        dt_str = [parts{2} ' ' parts{3}];
+        tag = join(parts(5:end),' ');
+        tag = tag{1};
+    catch
+        dt_str = '';
+        tag = '';
+    end
