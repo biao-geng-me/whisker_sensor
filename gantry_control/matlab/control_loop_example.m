0cbd99c93188bb2cb0851f9dd317d25022aae2e5
Biao Geng <biao.geng.me@gmail.com>
2026-03-13 15:45:17 -0400

    Test dummy agent

--- gantry_control/matlab/control_loop_example.m
+++ gantry_control/matlab/control_loop_example.m
@@ -8,14 +8,19 @@ n_rl_interval = 4;
 n_ch_total = 5+18; % t,x,y,u,v + 18 sensor channels
 STATE_DIM = n_rl_interval*n_ch_total;
 ACTION_DIM = 2;
-MAX_EPISODES = 3;
+MAX_EPISODES = 1;
 MAX_STEPS = 1000;
+SAMPLE_RATE = 80;
 
 config.mode = 'infer';
 config.hpc_port = 5555;
+config.n_rl_interval = n_rl_interval;
+config.n_ch_total = n_ch_total;
 config.state_dim = STATE_DIM;
 config.action_dim = ACTION_DIM;
 config.max_episodes = MAX_EPISODES;
+config.sample_rate = SAMPLE_RATE;
+config.dt = 1/SAMPLE_RATE;
 
 % 2. Initialize Network connection to Python
 try
@@ -34,10 +39,11 @@ for ep = 1:MAX_EPISODES
     
     % Hardware specific: Move robot to starting position
     % hardware.resetToHome();
-    currentState = rand(n_ch_total,n_rl_interval); % Dummy hardware read
-    currentState = currentState(:)';
+    currentState = zeros(n_ch_total,n_rl_interval); % Dummy hardware read
+    % currentState = currentState(:)';
     % Get first action from Python
-    action = net.startEpisode(currentState);
+    action = net.startEpisode(currentState(:)');
+    traj_hist = zeros(5,MAX_STEPS);
     
     for step = 1:MAX_STEPS
         
@@ -48,7 +54,13 @@ for ep = 1:MAX_EPISODES
         % --------------------------
         
         % Dummy Physics Update
-        currentState = currentState + norm(action) * 0.1; 
+        currentState = circshift(currentState, [0 -1]);
+        currentState(1,end) = step*config.dt*1000;
+        currentState(2:3,end) = action(1:2)' * config.dt * 1000 + currentState(2:3,end-1);
+        currentState(4:5,end) = action(1:2)';
+        currentState(6:end,end) = rand(config.n_ch_total-5,1);
+        traj_hist(:, step) = currentState(1:5, end); % Store trajectory history
+        
         reward = rand(); 
         
         % Check terminal condition (e.g., robot fell over or reached goal)
@@ -61,11 +73,12 @@ for ep = 1:MAX_EPISODES
         end
         
         % Query Python for the next action (with latency measurement)
-        tic;
-        action = net.stepRL(currentState, reward, done, truncated);
-        latency_ms = toc * 1000; % Convert to milliseconds
-        latencies = [latencies, latency_ms];
-        
+        if mod(step,config.n_rl_interval)==0
+            tic;
+            action = net.stepRL(currentState(:)', reward, done, truncated);
+            latency_ms = toc * 1000; % Convert to milliseconds
+            latencies = [latencies, latency_ms];
+        end
         if done > 0.5
             fprintf('Episode finished at step %d\n', step);
             break;
@@ -85,6 +98,14 @@ for ep = 1:MAX_EPISODES
         net.syncWithHPC();
     end
     
+    
+    t=traj_hist(1,:);
+    xt = traj_hist(2,:);
+    yt = traj_hist(3,:);
+    ut = traj_hist(4,:);
+    vt = traj_hist(5,:);
+    figure; plot(xt,yt);
+    figure; plot(t,ut); hold on; plot(t,vt);
 end
 
 % Phase 3: Teardown
--- gantry_control/python/agent_wrapper.py
+++ gantry_control/python/agent_wrapper.py
@@ -1,10 +1,13 @@
+import numpy as np
 class AgentWrapper:
     """Wraps the local DRL inference engine."""
     
     def __init__(self, config):
         self.config = config
-        self.state_dim = config.get("state_dim", 4)
+        self.state_dim = config.get("state_dim")
         self.action_dim = config.get("action_dim", 2)
+        self.n_rl_interval = config.get("n_rl_interval")
+        self.n_ch_total = config.get("n_ch_total")
         
         print(f"[Agent] Initialized with config: {self.config}")
         
@@ -37,8 +40,7 @@ class AgentWrapper:
         """Dummy CPU inference function."""
         # TODO: Load PyTorch/ONNX model and run forward pass here
         # For now, return a dummy action based on the state to prove it works
-        dummy_action = [state[0] * 0.1, state[1] * -0.1]
-        
+        dummy_action = self.dumb_agent(state)
         # Ensure it matches expected dimension
         while len(dummy_action) < self.action_dim:
             dummy_action.append(0.0)
@@ -53,3 +55,33 @@ class AgentWrapper:
         """Loads new neural network weights received from the HPC."""
         print(f"[Agent] Updating local weights... (Size: {len(new_weights)} bytes)")
         # TODO: Apply new weights to PyTorch/ONNX model
+
+    def dumb_agent(self, state):
+        """Dumb control"""
+        state_arr = np.array(state).reshape(self.n_rl_interval,self.n_ch_total)
+
+        t    = state_arr[:,0]
+        xloc = state_arr[:,1]
+        yloc = state_arr[:,2]
+        xvel = state_arr[:,3]
+        yvel = state_arr[:,4]
+
+        vel = 0.2
+        T = 5000
+        ycent = 400
+        a1 = 1.309 # 75°
+        t1 = ycent/np.sin(a1)/vel
+
+        if t[-1] < t1:
+            angle = a1
+            u_act = np.abs(np.cos(angle))*vel
+            v_act = np.sin(angle)*vel
+        else:
+            angle = 2*np.pi*((t[-1] - t1)/T) + a1
+            # u_act = vel*0.5 + np.abs(np.cos(angle))*vel*0.5
+            # v_act = np.sin(angle)*vel
+            u_act = np.abs(np.cos(angle))*vel
+            v_act = np.sin(angle)*vel
+
+        print(f'{t[-1]:12.1f},x={xloc[-1]:8.3f},y={yloc[-1]:8.3f},{angle:8.3f},{u_act:5.3f},{v_act:6.3f}')
+        return [u_act.item(), v_act.item()]
